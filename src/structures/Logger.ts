655f5ab874df4cb8a0d5ed8554844b90d6a694cf
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-01-01 19:41:47 +0100

    :bug: Logger all stdout

--- src/structures/Logger.ts
+++ src/structures/Logger.ts
@@ -1,11 +1,8 @@
 import { Guild, Message, User } from "discord.js";
 import { Command } from "./Command";
-import { Signale } from "signale";
-import { LambertDiscordClient } from "../client/LambertDiscordClient";
-import { init } from "../util/JavaScript";
+import { LambertDiscordClient } from "./LambertDiscordClient";
 import { LambertWebhookClient } from "./LambertWebhook";
-init();
-import inspector from "inspector";
+import "missing-native-js-functions";
 import { performance } from "perf_hooks";
 
 export interface Context {
@@ -16,34 +13,13 @@ export interface Context {
 
 export type LEVELS = "fatal" | "error" | "warn" | "info" | "debug";
 
+const log = console.log;
+console.log = function (...args: any[]) {
+	const location = new Error().stack.split("\n")[2].slice(4);
+	log.call(this, ...args, location);
+};
+
 export abstract class Logger {
-	public fatal(val: string, ctx?: Context): string {
-		return this.log("fatal", val, ctx);
-	}
-	public error(val: string, ctx?: Context): string {
-		return this.log("error", val, ctx);
-	}
-	public warn(val: string, ctx?: Context): string {
-		return this.log("warn", val, ctx);
-	}
-	public info(val: string, ctx?: Context): string {
-		return this.log("info", val, ctx);
-	}
-
-	public debug(val: string, ctx?: Context): string {
-		return this.log("debug", val, ctx);
-	}
-
-	protected log(level: string, val: string, ctx?: Context): string {
-		let str = "";
-		if (ctx) {
-			if (ctx.cmd) str += `[CMD]: ${ctx.cmd.id} `;
-			if (ctx.cmd) str += `[USER]: ${ctx.user?.tag} `;
-			if (ctx.cmd) str += `[GUILD]: ${ctx.guild?.name}(${ctx.guild?.id}) `;
-		}
-		return str + val;
-	}
-
 	static getColor(level: LEVELS) {
 		switch (level) {
 			case "fatal":
@@ -65,60 +41,43 @@ export abstract class Logger {
 	}
 }
 
-export type LoggerOptions = {
-	loggers?: Logger[];
-	level?: LEVELS;
-};
-
-export class LoggerCollection extends Logger {
-	public loggers: Logger[];
-
-	constructor(private opts: LoggerOptions = {}) {
-		super();
-		if (!opts.loggers) opts.loggers = [];
-		this.loggers = opts.loggers;
-		opts.loggers.push(new ChalkLogger(opts.level));
-	}
-
-	protected log(level: LEVELS, val: string, ctx?: Context): string {
-		this.loggers.map((logger) => logger[level](val, ctx));
-		return val;
-	}
-
-	public add(logger: Logger) {
-		this.loggers.push(logger);
-	}
-
-	public remove(logger: Logger) {
-		this.loggers.remove(logger);
-	}
+export function getLoggerLevelsAbove(level: LEVELS) {
+	let levels: LEVELS[] = ["debug", "info", "warn", "error", "fatal"];
+	return levels.slice(levels.indexOf(level));
 }
 
-export class LambertDiscordClientEventLogger {
+export type ClientEventLoggerOptions = {
+	level: LEVELS;
+};
+
+export class ClientEventLogger {
 	private start: number;
-	constructor(private client: LambertDiscordClient) {}
+	constructor(private client: LambertDiscordClient, private options: ClientEventLoggerOptions) {}
 
 	init() {
-		this.client.on("debug", this.debug);
-		this.client.on("error", this.error);
-		this.client.on("warn", this.warn);
-		this.client.on("ready", this.ready);
-		this.client.on("invalidated", this.invalidated);
-		this.info("[Bot] Starting");
+		if (!this.options) this.options = { level: "info" };
+		const levels = getLoggerLevelsAbove(this.options.level);
+		if (levels.includes("debug")) this.debug("[Bot] Starting");
+		if (levels.includes("debug")) this.client.on("debug", this.debug);
+		if (levels.includes("info")) this.client.on("ready", this.ready);
+		if (levels.includes("error")) this.client.on("error", this.error);
+		if (levels.includes("warn")) this.client.on("warn", this.warn);
+		if (levels.includes("fatal")) this.client.on("invalidated", this.invalidated);
+
 		this.start = performance.now();
 	}
 
 	debug = (x: string) => {
-		this.client.logger.debug(x);
+		console.debug(x);
 	};
 	error = (x: Error) => {
-		this.client.logger.error(x.toString());
+		console.error(x);
 	};
 	warn = (x: string) => {
-		this.client.logger.warn(x);
+		console.warn(x);
 	};
 	info = (x: string) => {
-		this.client.logger.info(x);
+		console.info(x);
 	};
 	invalidated = () => {
 		this.error(new Error(`Client Token is invalid`));
@@ -137,76 +96,53 @@ export class LambertDiscordClientEventLogger {
 	}
 }
 
-export type WebhookLoggerOptions = {
-	loggers: {
-		fatal?: string;
-		error?: string;
-		warn?: string;
-		debug?: string;
-		info?: string;
-	};
-	level: LEVELS;
-};
-
-export function getLoggerLevelsAbove(level: LEVELS) {
-	let levels: LEVELS[] = ["debug", "info", "warn", "error", "fatal"];
-	return levels.slice(levels.indexOf(level));
-}
-
 const DiscordFormatCharacters = /[_*`]/g;
 
-export class WebhookLogger extends Logger {
-	private webhooks: {
-		fatal?: LambertWebhookClient;
-		error?: LambertWebhookClient;
-		warn?: LambertWebhookClient;
-		debug?: LambertWebhookClient;
-		info?: LambertWebhookClient;
-	} = {};
+export class WebhookLogger {
+	private webhook: LambertWebhookClient;
 	private lastMessage: Message;
-	private level: LEVELS;
 
-	constructor(opts: WebhookLoggerOptions) {
-		super();
-		this.level = opts.level;
-		Object.keys(opts.loggers).forEach((key) => {
-			// @ts-ignore
-			this.webhooks[key] = new LambertWebhookClient(opts.loggers[key]);
-		});
+	constructor(webhook: string) {
+		if (!webhook) return;
+		this.webhook = new LambertWebhookClient(webhook);
 	}
 
-	protected log(level: LEVELS, val: string, ctx: Context = {}): string {
-		const above = getLoggerLevelsAbove(this.level);
-		if (!above.includes(level)) return val;
+	init() {
+		if (!this.webhook) return;
 
-		const aboveLevel = getLoggerLevelsAbove(level);
-		const fallback = aboveLevel
-			// @ts-ignore
-			.filter((x) => !!this.webhooks[x])
-			.first();
+		process.stdout.write = this.onLog.bind(this, process.stdout.write);
+		process.stderr.write = this.onError.bind(this, process.stderr.write);
+	}
 
-		level = this.webhooks[level] ? level : fallback;
-		const levelWebhook = this.webhooks[level];
-		if (!levelWebhook) return val;
+	onLog = (old: any, chunk: any): boolean => {
+		this.log("info", chunk.toString());
+		return old(chunk);
+	};
 
+	onError = (old: any, chunk: any): boolean => {
+		this.log("error", chunk.toString());
+		return old(chunk);
+	};
+
+	destroy() {
+		process.stdout.off("data", this.onLog);
+		process.stderr.off("data", this.onError);
+
+		this.webhook.destroy();
+		this.webhook = null;
+		this.lastMessage = null;
+	}
+
+	protected async log(level: LEVELS, val: string) {
 		const color = Logger.getHexColor(level);
-		if (ctx.cmd?.id) val += `[${ctx.cmd.id}] `;
 		val = val.replace(DiscordFormatCharacters, "\\$&");
-		let description = "```\n" + val.slice(0, 2039) + "\n```";
+		let description = "```\n" + val.slice(0, 2039) + "\n```"; // description limit: 2048
 
 		let options = {
 			embeds: [
 				{
 					description,
 					color,
-					author: {
-						name: ctx.guild ? `${ctx.guild.name} (${ctx.guild.id})` : undefined,
-						iconURL: ctx.guild?.iconURL({ dynamic: true, format: "jpg", size: 256 }) || undefined,
-					},
-					footer: {
-						text: ctx.user ? `${ctx.user.username} (${ctx.user.id})` : "",
-						iconURL: ctx.user?.displayAvatarURL({ dynamic: true, format: "jpg", size: 256 }),
-					},
 				},
 			],
 		};
@@ -217,46 +153,24 @@ export class WebhookLogger extends Logger {
 			this.lastMessage &&
 			// @ts-ignore
 			this.lastMessage.level === level &&
-			// @ts-ignore
-			this.lastMessage.ctx.equals(ctx) &&
 			(this.lastMessage?.embeds.first()?.description + description).length < 2048
 		) {
 			options.embeds.last().description =
 				"```\n" + this.lastMessage?.embeds.first()?.description?.slice(4, -4) + "\n" + val + "\n```";
 
+			promise = this.webhook.editMessage(this.lastMessage.id, options);
 			// @ts-ignore
-			promise = levelWebhook.editMessage(this.lastMessage.id, options);
-			// @ts-ignore
-			this.lastMessage = { ...this.lastMessage, embeds: options.embeds, level, ctx };
+			this.lastMessage = { ...this.lastMessage, embeds: options.embeds, level };
 		} else {
-			promise = levelWebhook.send(options).then((e) => {
+			promise = this.webhook.send(options).then((e) => {
 				// @ts-ignore
-				this.lastMessage = { ...e, level, ctx };
+				this.lastMessage = { ...e, level };
 			});
 		}
 
-		promise.catch((e) => {
-			console.error("there was an error sending logs to the webhook\n", e);
+		await promise.catch((e) => {
+			// CANT LOG -> infinite recursion
+			// console.error("there was an error sending logs to the webhook\n", e);
 		});
-
-		return val;
-	}
-}
-
-export class ChalkLogger extends Logger {
-	private signale: Signale;
-	constructor(private level: LEVELS = "info") {
-		super();
-		this.signale = new Signale({ logLevel: "info", stream: process.stdout });
-	}
-
-	log(level: LEVELS, val: string, ctx?: Context): string {
-		if (!getLoggerLevelsAbove(this.level).includes(level)) return val;
-		val = super.log(level, val, ctx);
-		// @ts-ignore
-		var debug = typeof v8debug === "object" || inspector.url();
-		if (debug) console.log(val);
-		else this.signale[level](val);
-		return val;
 	}
 }
